6baec22ccd1edd9f0a4dc47d6c6b1c74a051e8c0
ryantodd <ryantodd1176@gmail.com>
2021-06-08 19:11:39 +0300

    less1-2

---
+++ php/lesson2/tasks.php
@@ -0,0 +1,72 @@
+<?php
+// ЗАДАЧА 1
+// Дан массив $numbers
+$numbers = [5, 78, -200, 89, -14, 0, 73, -33, -4];
+
+// Объявить новый массив, в который войдут только положительные числа из $numbers
+
+// ЗАДАЧА 2
+// Дан массив $tours.
+$tours = [
+    [
+        'id' => 1,
+        'city' => 'Лондон',
+        'price' => 3600,
+        'country'=> 'Великобритания'
+    ],
+    [
+        'id' => 2,
+        'city' => 'Осло',
+        'price' => 2800,
+        'country'=> 'Норвегия'
+    ],
+    [
+        'id' => 3,
+        'city' => 'Сидней',
+        'price' => 4100,
+        'country'=> 'Австралия'
+    ],
+    [
+        'id' => 4,
+        'city' => 'Канберра',
+        'price' => 3900,
+        'country'=> 'Австралия'
+    ]
+];
+// Увеличить стоимость каждого тура на 10%. Для австралийских туров на 12%
+
+// ЗАДАЧА 3 (ДЗ)
+// Вывести в html информацию о товарах:
+$items = [
+    [
+        'id' => 1,
+        'title' => 'Flute',
+        'price' => 20000,
+        'img' => 'flute.jpg',
+        'description' => [
+            'color' => 'white',
+            'material' => 'bamboo'
+        ]
+    ],
+    [
+        'id' => 2,
+        'title' => 'Guitar',
+        'price' => 18000,
+        'img' => 'guitar.jpg',
+        'description' => [
+            'color' => 'brown',
+            'material' => 'linden'
+        ]
+    ],
+    [
+        'id' => 3,
+        'title' => 'Drum',
+        'price' => 68000,
+        'img' => 'drum.jpg',
+        'description' => [
+            'color' => 'brown',
+            'material' => 'steel'
+        ]
+    ],
+];
+
