ef6f840fe3a3cf7aa3c8f84f7167e9405bbab363
ryantodd <ryantodd1176@gmail.com>
2021-06-09 23:57:37 +0300

    HWDone

--- php/lesson2/lesson2.php
+++ php/lesson2/lesson2.php
@@ -16,42 +16,42 @@
 
 // ЗАДАЧА 2
 // Дан массив $tours.
-$tours = [
-    [
-        'id' => 1,
-        'city' => 'Лондон',
-        'price' => 3600,
-        'country'=> 'Великобритания'
-    ],
-    [
-        'id' => 2,
-        'city' => 'Осло',
-        'price' => 2800,
-        'country'=> 'Норвегия'
-    ],
-    [
-        'id' => 3,
-        'city' => 'Сидней',
-        'price' => 4100,
-        'country'=> 'Австралия'
-    ],
-    [
-        'id' => 4,
-        'city' => 'Канберра',
-        'price' => 3900,
-        'country'=> 'Австралия'
-    ]
-];
-// Увеличить стоимость каждого тура на 10%. Для австралийских туров на 12%
-
-foreach ($tours as $key => $tour){
-    if($tour['country'] != 'Австралия'){
-        $tour['price'] *= 1.10;
-    }elseif ($tour['country'] == 'Австралия') {
-        $tour['price'] *= 1.12;
-    }
-    var_dump($tour['price']);
-}
+//$tours = [
+//    [
+//        'id' => 1,
+//        'city' => 'Лондон',
+//        'price' => 3600,
+//        'country'=> 'Великобритания'
+//    ],
+//    [
+//        'id' => 2,
+//        'city' => 'Осло',
+//        'price' => 2800,
+//        'country'=> 'Норвегия'
+//    ],
+//    [
+//        'id' => 3,
+//        'city' => 'Сидней',
+//        'price' => 4100,
+//        'country'=> 'Австралия'
+//    ],
+//    [
+//        'id' => 4,
+//        'city' => 'Канберра',
+//        'price' => 3900,
+//        'country'=> 'Австралия'
+//    ]
+//];
+//// Увеличить стоимость каждого тура на 10%. Для австралийских туров на 12%
+//
+//foreach ($tours as $key => $tour){
+//    if($tour['country'] != 'Австралия'){
+//        $tour['price'] *= 1.10;
+//    }elseif ($tour['country'] == 'Австралия') {
+//        $tour['price'] *= 1.12;
+//    }
+//    var_dump($tour['price']);
+//}
 
 // Вывести в html информацию о товарах:
 $items = [
@@ -86,4 +86,37 @@ $items = [
         ]
     ],
 ];
+
+?>
+
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Товары</title>
+</head>
+<body>
+<section>
+    <? foreach ($items as $item): ?>
+    <div>
+        <h1>Товары</h1>
+        <h3>Название товара:<?= $item['title']?></h3>
+        <div>
+            <h4>Цена:<?= $item['price']?></h4>
+        </div>
+        <div>
+            <img src="img" alt="картинка" <?= $item['img']?>>
+        </div>
+        <div>
+            <h4>Цвет:<?= $item['description']['color']?></h4>
+            <h4>Материал:<?= $item['description']['material']?></h4>
+        </div>
+    </div>
+    <? endforeach; ?>
+</section>
+</body>
+</html>
 
